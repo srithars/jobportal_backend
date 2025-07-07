81daa219f31fd9350f7c2eda723e3215cabf9122
drkpn5848 <drkpn.in@gmail.com>
2025-07-07 22:17:52 +0530

    ss

--- src/main/java/klu/JobportalBackendApplication.java
+++ src/main/java/klu/JobportalBackendApplication.java
@@ -2,12 +2,12 @@ package klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JobportalBackendApplication {
+public class JobportalBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobportalBackendApplication.class, args);
 	}
-
 }
